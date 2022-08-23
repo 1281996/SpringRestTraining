32fcc8107a1d21d0f0f806dca000df6d8ee20d7a
unknown <cogjava3161@iiht.tech>
2022-08-23 15:28:22 +0530

    path variable

--- src/main/java/com/cog/controller/UserController.java
+++ src/main/java/com/cog/controller/UserController.java
@@ -2,8 +2,12 @@ package com.cog.controller;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +17,8 @@ import com.cog.service.UserService;
 
 @RestController
 public class UserController {
+	
+	Log LOGGER =LogFactory.getLog(UserController.class);
 	@Autowired
 	UserService userService;
 	//get all registered userss
@@ -24,7 +30,16 @@ public class UserController {
 	//user registration
 	@PostMapping("/register")
 	User saveUser(@RequestBody User user){
+		LOGGER.trace(user);
 		return userService.registerUser(user);
 		
 	}
+	//user path variable checking
+		@PostMapping("/user/age/{age}/height/{height}")
+		User saveUser(@RequestBody User user,@PathVariable("age") int age,@PathVariable("height") float height ){
+			LOGGER.trace(user);
+			System.out.println(age+"-"+height);
+			return null;
+			
+		}
 }
